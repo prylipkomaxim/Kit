a8ce0a36970c5644c4d8fda62f68d306ab8f0446
Maxim Prylipko <prilipko.maxim@gmail.com>
2022-06-07 09:59:59 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ if (menuBody && menuIcon) {
     menuIcon.addEventListener('click', function(){
         menuActive ();
 
-        if (menuIcon.classList.contains('burger-active')) {
+        if (menuIcon.classList.contains('burger-active') && body.classList.contains('_pc')) {
             body.style.paddingRight = '17px';
         } else {
             body.style.paddingRight = '0px';
@@ -74,3 +74,38 @@ window.addEventListener('resize', function() {
     }
 })
 
+
+var isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+                isMobile.Android()
+                || isMobile.BlackBerry()
+                || isMobile.iOS()
+                || isMobile.Opera()
+                || isMobile.Windows()
+                );
+    }
+};
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch')
+} else {
+    document.body.classList.add('_pc')
+}
+'use strict'
+
